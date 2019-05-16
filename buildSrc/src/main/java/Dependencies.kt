320684d96a7a96ef103e0a24477a70f73d751dc0
Nevin Chen <cnevinchen@gmail.com>
2019-05-16 10:10:31 +0800

    Closes #3373 - Add more Indian locales

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -52,5 +52,5 @@ object SystemEnv {
 }
 
 object Localization {
-    val KEPT_LOCALE = arrayOf("in", "hi-rIN", "th", "tl", "su", "jv", "vi", "zh-rTW")
+    val KEPT_LOCALE = arrayOf("in", "hi-rIN", "th", "tl", "su", "jv", "vi", "zh-rTW", "ta", "kn", "ml")
 }
