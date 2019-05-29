548af676d855b5f7db85399045278cdae1a8c471
Nevin Chen <cnevinchen@gmail.com>
2019-05-29 17:23:18 +0800

    play with setting loading animation

--- app/src/main/java/org/mozilla/rocket/content/news/NewsSettingFragment.kt
+++ app/src/main/java/org/mozilla/rocket/content/news/NewsSettingFragment.kt
@@ -1,15 +1,51 @@
 package org.mozilla.rocket.content.news
 
+import android.arch.lifecycle.Observer
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.FragmentActivity
 import android.support.v7.preference.PreferenceFragmentCompat
 import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.widget.ProgressBar
+import dagger.android.support.AndroidSupportInjection
 import org.mozilla.focus.R
+import org.mozilla.rocket.content.activityViewModelProvider
 import org.mozilla.rocket.content.news.data.CategorySetting
 import org.mozilla.rocket.content.news.data.NewsCatSettingCatAdapter
+import org.mozilla.rocket.content.news.data.NewsLanguagePreference
+import org.mozilla.rocket.content.news.data.NewsSettingsLocalDataSource
+import org.mozilla.rocket.content.news.data.NewsSettingsRemoteDataSource
+import org.mozilla.rocket.content.news.data.NewsSettingsRepository
+import org.mozilla.threadutils.ThreadUtils
+import java.lang.Thread.sleep
 
 class NewsSettingFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @javax.inject.Inject
+    lateinit var viewModelFactory: NewsViewModelFactory
+
+    @javax.inject.Inject
+    lateinit var applicationContext: Context
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        AndroidSupportInjection.inject(this)
+        val newsViewModel: NewsViewModel = activityViewModelProvider(viewModelFactory)
+        val newsSettingsRemoteDataSource = NewsSettingsRemoteDataSource()
+        val newsSettingsLocalDataSource = NewsSettingsLocalDataSource(applicationContext)
+        val newsLanguagePreference = findPreference("user_pref_lang") as NewsLanguagePreference
+        newsViewModel.newsSettingsRepository =
+            NewsSettingsRepository(newsSettingsRemoteDataSource, newsSettingsLocalDataSource)
+        newsViewModel.newsSettingsRepository.getLanguages().observe(viewLifecycleOwner, Observer {
+
+            newsLanguagePreference.updateLangList(it)
+        })
+        newsViewModel.newsSettingsRepository.getUserPreferenceLanguage().observe(viewLifecycleOwner, Observer {
+            newsLanguagePreference.summary = it?.name
+        })
+    }
 
     companion object {
         fun newInstance(): NewsSettingFragment {
@@ -23,6 +59,7 @@ class NewsSettingFragment : PreferenceFragmentCompat(), SharedPreferences.OnShar
 
     override fun onResume() {
         super.onResume()
+
         preferenceScreen.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
     }
 
@@ -33,11 +70,32 @@ class NewsSettingFragment : PreferenceFragmentCompat(), SharedPreferences.OnShar
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
-        if (key == "test2") {
+        // detect when the user had change the language setting, now refresh the categories
+        if (key == "user_pref_lang") {
+
+            // find hte view on activity... it's not pretty,
+            // TODO: we should let the update the cats via repository/use case, and let [NewsCategoryPreference] change it's data
             val list = (context as? FragmentActivity)?.findViewById<RecyclerView>(R.id.news_setting_cat_list)
+            val progress = (context as? FragmentActivity)?.findViewById<ProgressBar>(R.id.news_setting_cat_progress)
+
+            list?.visibility = View.GONE
+            progress?.visibility = View.VISIBLE
+
             (list?.adapter as? NewsCatSettingCatAdapter)?.apply {
-                this.cats = listOf(CategorySetting("D", "DD"), CategorySetting("E", "EE"), CategorySetting("F", "FF"))
-                this.notifyDataSetChanged()
+                ThreadUtils.postToBackgroundThread {
+                    sleep(3000)
+                    ThreadUtils.postToMainThread {
+                        this.cats =
+                            listOf(
+                                CategorySetting("D", "DD"),
+                                CategorySetting("E", "EE"),
+                                CategorySetting("F", "FF")
+                            )
+                        this.notifyDataSetChanged()
+                        list.visibility = View.VISIBLE
+                        progress?.visibility = View.GONE
+                    }
+                }
             }
         }
     }
